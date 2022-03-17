60360c3310e470e84a3c057cbc1fe49777fb73ea
Bcgg <liubenchen18@163.com>
2022-03-17 10:54:22 +0800

    done test1

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Python Project
 ## Logs 
 ### 2022/03/17
-1. init project 
+1. init project 1 
+2. done
--- test1/1.py
+++ test1/1.py
@@ -1,5 +1,10 @@
 import re
 
+def find_func(str):
+    pa = r'([\u4e00-\u9fa5]*)(\u53a6\u95e8)([\u4e00-\u9fa5]*)(\s|)([0-9]*)([\u4e00-\u9fa5]*)'
+    org = re.search(pa,str,re.U)
+    return org.group()
+
 def func(mystr,pattern) ->list:
     ret = []
     next_str = re.search(pattern,mystr,re.U)
@@ -17,6 +22,8 @@ def main():
     str = input()
     pattern = r'(([\u4e00-\u9fa5])*(\s)*)([\u4e00-\u9fa5])(:|：)((\d{3}-\d{8}|\d{4}-\d{7}|\d{7})(\s*)(,|，))*(\d{3}-\d{8}|\d{4}-\d{7}|\d{7})'
     number_list = func(str,pattern)
+    org = find_func(str)
+    print(org)
     for number in number_list:
         print(number)
 
