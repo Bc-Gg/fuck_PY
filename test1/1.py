4f9c5c88b08b46dcf7f1f95de070883ca3288afd
Bcgg <liubenchen18@163.com>
2022-03-17 11:52:44 +0800

    fix some bugs in test1

--- test1/1.py
+++ test1/1.py
@@ -3,7 +3,9 @@ import re
 def find_func(str):
     pa = r'([\u4e00-\u9fa5]*)(\u53a6\u95e8)([\u4e00-\u9fa5]*)(\s|)([0-9]*)([\u4e00-\u9fa5]*)'
     org = re.search(pa,str,re.U)
-    return org.group()
+    if org:
+        return org.group()
+    return None
 
 def func(mystr,pattern) ->list:
     ret = []
@@ -12,18 +14,21 @@ def func(mystr,pattern) ->list:
     while True:
         ret.append(ss)
         mystr = mystr[next_str.span()[1]:]
+        # print(f'mystr:{mystr}')
         if re.search(pattern, mystr, re.U) == None:
             break
-        ss = re.search(pattern, mystr, re.U).group()
+        next_str = re.search(pattern, mystr, re.U)
+        ss = next_str.group()
     return ret
-
 
 def main():
     str = input()
-    pattern = r'(([\u4e00-\u9fa5]|[a-zA-z])*(\s)*)([\u4e00-\u9fa5]|[a-zA-z])(:|：)((\d{11})|((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})))'
-    number_list = func(str,pattern)
-    org = find_func(str)
+    sstr = str
+    pattern = r'(([\u4e00-\u9fa5]|[a-zA-z])*(\s)*)([\u4e00-\u9fa5]|[a-zA-z])(:|：)((((\d{11})|((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})))(\s*)(,|，))*)((\d{11})|((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})))'
+    org = find_func(sstr)
+    number_list = list(set(func(str,pattern)))
     print(org)
+    # print(number_list)
     for number in number_list:
         print(number)
 
